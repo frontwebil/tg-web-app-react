8f7b5cbd95177d6111146eda5e37f210545023e5
Illya Sukhomlyn <gollpfd@gmail.com>
2026-09-06 20:50:32 +0300

    update

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -55,7 +55,7 @@ export function ProductList() {
       queryId
     };
 
-    fetch("http://localhost:8000", {
+    fetch("https://tg-web-app-bot-ur6t.onrender.com/web-data", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
